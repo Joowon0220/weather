29aec2c75ab7291c881901647937873968fbf482
joowon0220 <one.joowon@gmail.com>
2020-05-21 16:18:04 +0900

    weatherAPI project

---
+++ weather_03/src/com/weather03/mvc/VillageWeatherDAO.java
@@ -0,0 +1,68 @@
+package com.weather03.mvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static com.weather03.db.JDBCTemplate.*;
+
+public class VillageWeatherDAO {
+
+	public int insertVillageWeather(List<VillageWeather> vw) throws ClassNotFoundException {
+
+		List<VillageWeather> vgw = new ArrayList<VillageWeather>();
+		Connection con = null;
+		ResultSet rs = null;
+		PreparedStatement insert_pstm = null;
+		int res = 0;
+
+		con = getConnection();
+
+
+
+
+		String query = "INSERT INTO WEATHER03" + " VALUES(getdate(),?,?,?,?)";
+
+		try {
+			for (int i = 0; i < vw.size(); i++) {
+				VillageWeather str = vw.get(i);
+
+				insert_pstm = con.prepareStatement(query);
+				insert_pstm.setString(1, str.getBaseDate());
+				insert_pstm.setString(2, str.getBaseTime());
+				insert_pstm.setString(3, str.getT3h());
+				insert_pstm.setString(4, str.getReh());
+
+				System.out.println("3.query ready");
+				res = insert_pstm.executeUpdate();
+
+				if (res > 0) {
+					commit(con);
+					System.out.println("인서트 성공");
+				}
+
+				
+				
+				System.out.println("4.return");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("3 or 4 error");
+			e.printStackTrace();
+		} finally {
+			close(insert_pstm, con);
+		}
+		return res;
+		
+		
+
+		// else 끝
+
+	}
+}
